72118ec9db2e53de1304a61578eb6d95eaa8e57c
BlackMarine <wkdeo0000@gmail.com>
2020-04-14 18:53:21 +0900

    BLD: 1.4 ~ 1.8 Complete https://youtu.be/Z27sK7sNoSk

--- 1.0~1.3.py
+++ 1.0~1.3.py
@@ -57,3 +57,67 @@ print(type(n_age)) # str 18
 
 nico2 = {age:44} 
 print(nico2)
+
+#1.4 함수 정의 하는방법
+def say_hello():
+  print("hello")
+  print("bye")
+say_hello()
+say_hello()
+
+
+#1.5 def 함수 옆 (argument인자)
+#def say_hello(who)):
+#  print("hello", who)
+#
+#say_hello()
+
+def say_hello(name="anonymous"): #default 값설정하기
+  print("hello",name)
+
+say_hello()
+say_hello("nico")
+
+#1.6 return 함수결과를 저장하고싶을때
+print("@@@@@@@@@@@@@@@@@@")
+def p_plus (a, b):
+  print(a + b)
+
+def r_plus(a, b):
+  return a + b
+  print("리턴한 후 함수는 종료됨 이거 출력 안됨")
+
+
+p_result = p_plus(2, 3) #프린트 p_plus를 했을 뿐 저장x
+r_result = r_plus(2, 3) #return a + b 이거로 값을 저장함
+
+print(p_result, r_result)
+   #       None   5
+
+#1.7 하트
+#1.8 keyworded Arguments
+
+def plus (a, b):
+  return a+b;
+
+result = plus(2,4)
+print(result)
+
+# 키워드 알규먼트로 인자(argument) 이용가능
+result = plus(b=30, a=1)
+print(result)
+
+
+#string 앞에 f 붙여서 format 하기
+def say_hello2(name, age):
+  return "hello name you are age years old"
+
+hello = say_hello2("nico","12")
+print(hello)
+
+# name 이랑 age를 string이 아닌 f 랑 {}로 사용해야함
+def say_hello3(name, age):
+  return f"hello {name} you are {age} years old"
+
+hello = say_hello3(name = "nico",age="12")
+print(hello)
